6a458f2cc1fa67d882c2d55bafc424ee9b0286d5
若山侑輝 <wkym_4a@icloud.com>
2019-06-16 18:39:59 +0900

    fix_keysをec2用に修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
 server '13.115.176.5', user: 'ec2-user', roles: %w{app db web}
-set :ssh_options, keys: '/Users/wakayamayuki/.ssh/id_rsa'
+set :ssh_options, keys: [File.expand_path('/Users/wakayamayuki/workspace/key_management/mappura-4-key.pem')]
+# set :ssh_options, keys: '/Users/wakayamayuki/.ssh/id_rsa'
