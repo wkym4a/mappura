3d989c458bdee9c99b94f02aa0048af670446f68
若山侑輝 <wkym_4a@icloud.com>
2019-06-16 17:40:43 +0900

    init unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '54.250.200.211', user: 'app', roles: %w{app db web}
+server '13.115.176.5', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, keys: '/Users/wakayamayuki/.ssh/id_rsa'
