dc6c1da1740397b0a2ed83ea927568f5709f7803
若山侑輝 <wkym_4a@icloud.com>
2019-07-02 23:34:11 +0900

    fix_メール送信のため、aws.rbの記述を変更

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.add_delivery_method(
   :ses,
   AWS::SES::Base,
-  access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-  secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   server: 'email.us-west-2.amazonaws.com'
 )
